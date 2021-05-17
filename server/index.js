e0a739ce94dc51bdbc13b4fd762b1f3a5a48fff7
DISHEN MAKWANA <dishenmakwana.dm@gmail.com>
2021-05-17 12:30:58 +0530

    deploy the app
    Signed-off-by: DISHEN MAKWANA <dishenmakwana.dm@gmail.com>

---
+++ server/index.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/user.js';
+
+dotenv.config();
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
+    next();
+});
+
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('Hello from Memories API');
+    });
+}
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+console.log(process.env.CONNECTION_URL);
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    )
+    .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
