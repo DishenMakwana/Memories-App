c57d6cc7a450c8aecf8fdb984bb0e18202d5032a
DISHEN MAKWANA <dishenmakwana.dm@gmail.com>
2021-05-17 12:43:57 +0530

    build folder path change
    Signed-off-by: DISHEN MAKWANA <dishenmakwana.dm@gmail.com>

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     );
